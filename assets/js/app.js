a118f90497343730dbfa828acc476bbb2f38c2ea
ui-dev-chris <119463904+ui-dev-chris@users.noreply.github.com>
2022-12-01 13:38:47 +0000

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,53 +4,30 @@
 // jQuery
 $(document).ready(function() {
 
-    $(function() { 
-	
-        var windowHeight= $(window).height();
-        if(windowHeight < 920){
-            
-            // masquer le menu-toggle
-            $('#navigation .menu').hide();
-            
-            // clicker sur le btn-toggle
-            $('#navigation .btn-toggle').click( function(e){
-
-                // afficher le menu-toggle
-                $('#navigation .menu').show();
-
-                $('#navigation.user a.lien').show();
-                
-                e.preventDefault();
-                
-            } )
-
-            // clicker sur le btn-ferme
-            $('#navigation .btn-ferme').click( function(e){
-
-                // ferme le menu
-                $('#navigation .menu').hide();
-
-                e.preventDefault();
-                
-            } )
-
-            // clicker sur les lien du menu
-            $('#navigation .menu a.lien').click( function(e){
-
-                // ferme le menu
-                $('#navigation .menu').hide();
-
-
-                //e.preventDefault();
-                
-            } )
-
-        }
-           
-    });
+    $('#navigation .menu').hide();
     
 
+    // afficher le menu et le btn-fermee 
+    $('#navigation .btn-toggle').click(function(e){
+        
+        $('#navigation .menu').show();
+        $('#navigation .user a.lien').show();
+        e.preventDefault();
 
+    })
+
+    $('#navigation .btn-ferme').click(function(e){
+        
+        $('#navigation .menu').hide();
+        e.preventDefault();
+
+    })
+
+    // lorsqu'on click sur un lien du menu
+    $('#navigation .menu a.lien').click(function(e){
+        
+        $('#navigation .menu').hide();
+
+    })
     
-
 })
